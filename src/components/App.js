3069b54767f2a80454ad24609d65913a3715d5d3
Viikas Batham <112746619+vikasbatham8@users.noreply.github.com>
2023-02-14 21:27:04 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,25 @@
-import React from 'react'
-import '../styles/App.css';
-const arr = JSON.parse(window.localStorage.getItem('props') || `["hello","world"]`) // do not change
-const Join = (props) =>{
-  return(
-    <div id ="join">
+
+import React from "react";
+import "../styles/App.css";
+const arr = JSON.parse(
+  window.localStorage.getItem("props") || `["hello","world"]`
+); // do not change
+
+const Join = (props) => {
+  return (
+    <div id="join">
+      {props.words.join(",")}
       {/* Access prop 'words' and print it using .join like words.join(',')*/}
     </div>
-  )
-}
-const App = () => {
+  );
+};
 
+const App = () => {
   return (
     <div id="main">
-      <Join  />
+      <Join words={arr} />
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
